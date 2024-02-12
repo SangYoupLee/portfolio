97a287525f2e6ef7e5fd0f080073c3636ae7f2f9
SangYoupLee <125184499+SangYoupLee@users.noreply.github.com>
2024-02-12 06:14:53 +0000

    git add src/App.jsx git add src/Components/About.jsx git add src/Components/Home.jsx git add src/Components/Portfolio.jsx git add src/images/explorer.jpg git add src/images/marketing.jpg git add src/images/meeting.jpg origin/main

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/meeting.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "meeting with team";
 
 /**
  * Project list
@@ -31,28 +31,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Make a web page with Blazor (C#) 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "using dotnet framework, made a pizza store web page..",
+    url: "https://github.com/hackersground-kr/blazor-workshop-prototype",
   },
 ];
 
